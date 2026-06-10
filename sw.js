bb6f1c3c566f411976b8d48e570a0a15ff9ee00f
Jun Zhu <gyzj09@gmail.com>
2026-06-10 12:56:26 +0000

    PWA: installable app (manifest, service worker, icons, install button)

---
+++ sw.js
@@ -0,0 +1,11 @@
+const C='dealboard-v1';
+const ASSETS=['./','./index.html','./icon-192.png','./icon-512.png','./manifest.webmanifest'];
+self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
+self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
+self.addEventListener('fetch',e=>{
+  const r=e.request; if(r.method!=='GET') return;
+  e.respondWith(
+    fetch(r).then(res=>{ const cc=res.clone(); caches.open(C).then(c=>c.put(r,cc)); return res; })
+    .catch(()=>caches.match(r).then(m=>m||caches.match('./index.html')))
+  );
+});
