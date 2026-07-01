74f8bd7529c2c8d6b76b09e2fcd4d0af163f5cc6
Jun <jun@yongfamach.com>
2026-07-01 17:52:55 +0800

    All-Time totals now auto-compute live from client data (value/collected/outstanding always exact); 2026 row reconciles so table foots; SW v15

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const C='dealboard-v14';
+const C='dealboard-v15';
 const ASSETS=['./','./index.html','./icon-192.png','./icon-512.png','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(C).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting()));});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.filter(k=>k!==C).map(k=>caches.delete(k)))).then(()=>self.clients.claim()));});
